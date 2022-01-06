b7342eee64d7f9e6fbe13714bd38f027dc409e11
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2022-01-06 18:42:07 +0700

    flasher: Add detected product model to kernel command line
    Since we will be adding Willow device support, so add product
    model detector to kernel command line which is useful for NFC.
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- flasher/anykernel.sh
+++ flasher/anykernel.sh
@@ -48,6 +48,14 @@ else
   patch_cmdline "androidboot.version" ""
 fi
 
+# Set product hardware for kernel
+sku="$(file_getprop /system/build.prop ro.build.product)"
+if [ ! -z "$sku" ]; then
+  patch_cmdline "androidboot.product.hardware.sku" "androidboot.product.hardware.sku=$sku"
+else
+  patch_cmdline "androidboot.product.hardware.sku" ""
+fi
+
 # end ramdisk changes
 
 flash_boot;
